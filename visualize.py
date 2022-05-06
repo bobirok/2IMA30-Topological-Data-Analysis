3e3099946f94e29751bf27a69e92f6f5a5d365a6
bobirok <bobirok@abv.bg>
2022-05-06 21:02:50 +0200

    filter lat long

--- visualize.py
+++ visualize.py
@@ -1,15 +1,44 @@
+from pickle import TRUE
 from turtle import color
 from numpy import size
-import pandas
+import pandas as pd
 import matplotlib.pyplot as plt
+import os
+import glob
+
+bounding_box = {
+    "x1": 116.21200561523438,
+    "x2": 116.54159545898438,
+    "y1": 39.77265852521458,
+    "y2": 40.02340800226773
+}
+
+def read_files():
+    schema = ["lat", "long", "zeroes", "altitude", "dateNr", "date", "time"]
+    path = os.getcwd()
+
+    df = None
+    for i in range(161, 163):
+        directory = './Data/{:03d}/Trajectory'.format(i)
+        csv_files = glob.glob(os.path.join(os.path.join(path, directory), "*.plt"))
+        print("User {} has {} files".format(i, len(csv_files)))
+        for file in csv_files:
+            if(df is not None):
+                df = pd.concat([df.copy(), (pd.read_csv(file, skiprows=6, names=schema))], axis=0, ignore_index=True)
+            else:
+                df = pd.read_csv(file, skiprows=6, names=schema)
+    return df
 
 def main():
-    schema = ["lat", "long", "zeroes", "altitude", "dateNr", "date", "time"]
-    for i in range(51):
-        print(f"reading: {i}")
-        df = pandas.read_table(f"./sample_trajectories/{i+1}.plt", names=schema, quotechar = "\"", skiprows = 7, sep = ",")
-        putDFOnPlot(df)
-    plt.show()
+    df = read_files()
+    x1 = bounding_box["x1"]
+    x2 = bounding_box["x2"]
+    y1 = bounding_box["y1"]
+    y2 = bounding_box["y2"]
+
+    new_df = df.loc[((df["lat"] >= y1) & (df["lat"] <= y2)) & ((df["long"] >= x1) & (df["long"] <= x2))]
+    print(len(df), len(new_df))
+
 
 def putDFOnPlot(df):
     plt.scatter(x=df['long'], y=df['lat'], color="red", s=.3**2)
