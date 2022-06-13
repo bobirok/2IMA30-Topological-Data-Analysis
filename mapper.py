fd822ec81a2305df6fa840a1f10afaed4fe51efe
Gogo <g.t.kostov@student.tue.nl>
2022-06-13 10:15:41 +0300

    adding mapper

---
+++ mapper.py
@@ -0,0 +1,29 @@
+import kmapper as km
+import pandas as pd
+import sklearn
+
+df = pd.read_csv("torus_coordinates_1000.csv")
+
+data = df.drop(columns=["timestamp"]).to_numpy()
+
+
+# Initialize
+mapper = km.KeplerMapper(verbose=2)
+
+# Fit to and transform the data
+projected_data = mapper.fit_transform(data, projection=[2])
+
+
+clusterer = sklearn.cluster.DBSCAN(eps=0.3, min_samples=5)
+cover = km.Cover(n_cubes=20, perc_overlap=0.3)
+
+# Create dictionary called 'graph' with nodes, edges and meta-information
+graph = mapper.map(projected_data, data, cover=cover, clusterer=clusterer)
+
+labels = data[:, 2]
+# Visualize it
+mapper.visualize(graph, path_html="make_circles_keplermapper_output.html",
+                 title="Torus Map Y", color_values=labels, color_function_name="labels")
+
+if __name__ == '__main__':
+    pass
