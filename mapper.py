5eb1dee03eae048b377efd965bef388a9e871cad
Weijts <k.weijts@student.tue.nl>
2022-06-26 20:04:30 +0200

    fixed lenses mapper

--- mapper.py
+++ mapper.py
@@ -52,23 +52,27 @@ def create_lenses(df):
 
 data_path = "lens_data.csv"
 if os.path.exists(data_path):
-    data = np.genfromtxt(data_path, delimiter=',') #, dtype=None
+    #data = np.genfromtxt(data_path, delimiter=',',dtype=None) #, dtype=None
+    data = pd.read_csv("lens_data.csv", names=['traj', 'mean', 'std', 'label'])
 else:
     print('load has failed')
     data = pd.read_csv("test_data_filtered.csv")
     data = create_lenses(data)
     np.savetxt(data_path, data,  fmt="%s, %s, %s, %s", delimiter=",")
 
-print(data[0:5])
+print(data)
 #
-# lens1 = data[:, [1, 2]]
-data_names = data[:, 3]
+
+data_names = data['label'].to_numpy()
 # data = data[:, [0, 1, 2]]
 
 # print(lens1)
 
-test_data = data[:, [1,2]]
-test_data = test_data[~np.isnan(test_data).any(axis=1), :]
+test_data = data[['mean', 'std']].astype({"mean": float, 'std': float}, errors='raise').to_numpy()
+test_data = np.nan_to_num(test_data)
+lens1 = test_data[:, [0, 1]]
+print(data_names)
+print(type(data_names[0]))
 
 # Initialize
 mapper = km.KeplerMapper(verbose=2)
@@ -78,18 +82,18 @@ lens2 = mapper.fit_transform(test_data, projection=[0,1])
 #projected_data = mapper.fit_transform(data, projection=[0,1])
 # lens = np.c_[lens1, lens2]
 
-#clusterer = sklearn.cluster.DBSCAN(eps=1000000, min_samples=1)
-clusterer=sklearn.cluster.KMeans(n_clusters=2,random_state=3471)
+clusterer = sklearn.cluster.DBSCAN(eps=1, min_samples=2)
+#clusterer=sklearn.cluster.KMeans(n_clusters=2,random_state=3471)
 cover = km.Cover(n_cubes=15, perc_overlap=0.7)
 
 # Create dictionary called 'graph' with nodes, edges and meta-information
-graph = mapper.map(lens2, test_data, cover=cover, clusterer=clusterer)
+graph = mapper.map(lens1, test_data, cover=cover, clusterer=clusterer)
 
-labels = test_data[:, 1]
+labels = test_data[:, 0]
 print(graph)
 # Visualize it
 mapper.visualize(graph, path_html="make_circles_keplermapper_output.html",
-                 title="Torus Map Y", color_values=labels, color_function_name="labels", X_names=data_names)
+                 title="Torus Map Y", color_values=labels, color_function_name="labels", custom_tooltips=data_names, X=test_data, X_names=['mean', 'std'])
 
 if __name__ == '__main__':
     pass
